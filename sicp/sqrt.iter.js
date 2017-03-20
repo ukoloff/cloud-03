4ce716abf4dfecc0fdc28686b4c73942c77facfc
Stas Ukolov <ukoloff@gmail.com>
2017-03-20 02:02:56 -0700

    Iterative sqrt()

---
+++ sicp/sqrt.iter.js
@@ -0,0 +1,38 @@
+
+function sqrt(x)
+{
+  for(var approx = 1; !is_good(approx); approx = improve(approx));
+  return approx
+
+  function square(x)
+  {
+    return x * x
+  }
+
+  function is_good(approx)
+  {
+    return abs(square(approx) - x) < 1e-3
+  }
+
+  function abs(x)
+  {
+    return x > 0 ? x : -x
+  }
+
+  function improve(approx)
+  {
+    return avg(approx, x / approx)
+  }
+
+  function avg(a, b)
+  {
+    return (a + b) / 2
+  }
+
+  function iter(approx)
+  {
+    return is_good(approx, x) ? approx : iter(improve(approx, x), x)
+  }
+}
+
+console.log("sqrt(5) = ", sqrt(5))
