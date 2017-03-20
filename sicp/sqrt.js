64087e68fa34bf17b976cc995585d8733e79bfe3
Stas Ukolov <ukoloff@gmail.com>
2017-03-20 01:55:21 -0700

    Use hoisting

--- sicp/sqrt.js
+++ sicp/sqrt.js
@@ -1,19 +1,37 @@
 
 sqrt = (x) =>
 {
-  let square = x => x * x
-
-  const is_good = (approx, x) => abs(square(approx) - x) < 1e-3
-
-  var abs = (x) => x > 0 ? x : -x
-
-  let improve = (approx, x) => avg(approx, x / approx)
-
-  let avg = (a, b) => (a + b) / 2
-
-  let iter = (approx, x) => is_good(approx, x) ? approx : iter(improve(approx, x), x)
-
   return iter(1, x)
+
+  function square(x)
+  {
+    return x * x
+  }
+
+  function is_good(approx, x)
+  {
+    return abs(square(approx) - x) < 1e-3
+  }
+
+  function abs(x)
+  {
+    return x > 0 ? x : -x
+  }
+
+  function improve(approx, x)
+  {
+    return avg(approx, x / approx)
+  }
+
+  function avg(a, b)
+  {
+    return (a + b) / 2
+  }
+
+  function iter(approx, x)
+  {
+    return is_good(approx, x) ? approx : iter(improve(approx, x), x)
+  }
 }
 
 console.log("sqrt(5) = ", sqrt(5))
