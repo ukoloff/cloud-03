101f6cc695e1e9ea4b508b0d1843210f452f409d
Stas Ukolov <ukoloff@gmail.com>
2017-04-21 13:03:18 +0500

    Derive method

---
+++ sicp/lambda/newton.derive.js
@@ -0,0 +1,36 @@
+function newton(f, start)
+{
+  const df = derive(f)
+  return iter(start)
+
+  function iter(x)
+  {
+    return isGood(x) ? x : iter(improve(x))
+  }
+
+  function improve(x)
+  {
+    return x - f(x) / df(x)
+  }
+
+  function isGood(x)
+  {
+    return abs(f(x)) < 1e-5
+  }
+
+  function abs(x)
+  {
+    return x > 0 ? x : -x
+  }
+}
+
+function derive(f)
+{
+  const dx = 1e-3
+  return function(x)
+  {
+    return (f(x + dx) - f(x)) / dx
+  }
+}
+
+console.log("sqrt(2) =", newton(x => x * x - 2, 1))
