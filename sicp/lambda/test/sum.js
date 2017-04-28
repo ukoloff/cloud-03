19911eee2fabe971188850aec428e4758766ca16
Stas Ukolov <ukoloff@gmail.com>
2017-04-28 12:18:08 +0500

    Pi

--- sicp/lambda/test/sum.js
+++ sicp/lambda/test/sum.js
@@ -1,6 +1,7 @@
 const sumInts = require('../sum/int')
 const sumCubes = require('../sum/cubes')
+const sumPi = require('../sum/pi')
 
 console.log("Sum 1..5 =", sumInts(1, 5))
 console.log("Sum of cubes 1..5 =", sumCubes(1, 5))
-// console.log("Pi 1..5 =", 8 * sumPi(1, 100))
+console.log("Pi 1..5 =", 8 * sumPi(1, 100))
